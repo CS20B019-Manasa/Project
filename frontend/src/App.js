3fd2f7d27642b3b1fdbdc4fecbeba7191a4ac23f
CS20B019-Manasa <cs20b019@iittp.ac.in>
2023-03-15 15:42:55 +0530

    added some products to home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import data from './data';
 
 function App() {
   return (
@@ -5,6 +6,27 @@ function App() {
       <header>
         <a href="/">VMart</a>
       </header>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>INR {product.price}</strong>
+                </p>
+                <button>Add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
